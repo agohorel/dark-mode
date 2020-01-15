d79178266f42fd2cc7a9af1e7ff3b5d750f13de2
agohorel <gohorelalex@gmail.com>
2020-01-15 15:28:37 -0500

    dark mode mvp

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useDarkMode = (key, initialValue) => {
+  const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    console.log(body);
+    console.log(body.classList);
+    darkMode
+      ? body.classList.add("dark-mode")
+      : body.classList.remove("dark-mode");
+  }, [darkMode]);
+
+  return [darkMode, setDarkMode];
+};
